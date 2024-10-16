e6f37783a7da060016f6e9dc0dd3a21afb1c543d
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-10-16 22:21:20 +0200

    sound with distances

---
+++ src/lib.rs
@@ -0,0 +1,168 @@
+#![allow(clippy::precedence)]
+
+use std::sync::{Arc, RwLock};
+
+use assert_no_alloc::*;
+use cpal::traits::{DeviceTrait, StreamTrait};
+use cpal::{FromSample, SizedSample};
+use crossbeam_channel::{Receiver, Sender};
+use fundsp::hacker::*;
+use nalgebra::Vector3;
+
+#[cfg(debug_assertions)] // required when disable_release is set (default)
+#[global_allocator]
+static A: AllocDisabler = AllocDisabler;
+
+const SOUND_SPEED: f32 = 343.0;
+pub const HEAD_RADIUS: f32 = 0.10;
+#[derive(Clone)]
+pub struct SourceInfo {
+    pub relative_position: Vector3<f32>,
+    pub direction: Vector3<f32>,
+}
+
+impl Default for SourceInfo {
+    fn default() -> Self {
+        SourceInfo {
+            relative_position: Vector3::new(0.0, 0.0, 0.0),
+            direction: Vector3::new(1.0, 0.0, 0.0),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct InputNode {
+    receiver: Receiver<(f32, f32)>,
+}
+
+impl InputNode {
+    pub fn new(receiver: Receiver<(f32, f32)>) -> Self {
+        InputNode { receiver }
+    }
+}
+
+impl AudioNode for InputNode {
+    const ID: u64 = 87;
+    type Inputs = U0;
+    type Outputs = U2;
+
+    #[inline]
+    fn tick(&mut self, _input: &Frame<f32, Self::Inputs>) -> Frame<f32, Self::Outputs> {
+        let (left, right) = self.receiver.try_recv().unwrap_or((0.0, 0.0));
+        [left, right].into()
+    }
+}
+
+pub fn run_in<T>(device: &cpal::Device, config: &cpal::StreamConfig, sender: Sender<(f32, f32)>)
+where
+    T: SizedSample,
+    f32: FromSample<T>,
+{
+    let channels = config.channels as usize;
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+    let stream = device.build_input_stream(
+        config,
+        move |data: &[T], _: &cpal::InputCallbackInfo| read_data(data, channels, sender.clone()),
+        err_fn,
+        None,
+    );
+
+    if let Ok(stream) = stream {
+        if let Ok(()) = stream.play() {
+            std::mem::forget(stream);
+        }
+    }
+    println!("Input stream built.");
+}
+
+fn read_data<T>(input: &[T], channels: usize, sender: Sender<(f32, f32)>)
+where
+    T: SizedSample,
+    f32: FromSample<T>,
+{
+    for frame in input.chunks(channels) {
+        let mut left = 0.0;
+        let mut right = 0.0;
+        for (channel, sample) in frame.iter().enumerate() {
+            if channel & 1 == 0 {
+                left = sample.to_sample::<f32>();
+            } else {
+                right = sample.to_sample::<f32>();
+            }
+        }
+        if let Ok(()) = sender.try_send((left, right)) {}
+    }
+}
+
+pub fn run_out<T>(
+    device: &cpal::Device,
+    config: &cpal::StreamConfig,
+    receiver: Receiver<(f32, f32)>,
+    source_info: Arc<RwLock<SourceInfo>>,
+) -> Result<(), anyhow::Error>
+where
+    T: SizedSample + FromSample<f32> + Send,
+{
+    let input = An(InputNode::new(receiver));
+
+    let sample_rate = config.sample_rate.0 as f64;
+    let channels = config.channels as usize;
+    let amplitude: Shared = shared(1.0);
+
+    let mut net = Net::wrap(Box::new(input));
+    net.set_sample_rate(sample_rate);
+    net.chain(Box::new(tick() * var(&amplitude)));
+    
+    net.check();
+    println!("Net checked.");
+    let backend = net.backend();
+
+    let mut backend = BlockRateAdapter::new(Box::new(backend));
+
+    // Use `assert_no_alloc` to make sure there are no allocations or deallocations in the audio thread.
+    let mut next_value = move || assert_no_alloc(|| backend.get_stereo());
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+    let mut updated_source_info = SourceInfo::default();
+    loop {
+        if let Ok(info) = source_info.try_read() {
+            updated_source_info = info.clone();
+            let distance = updated_source_info.relative_position.norm();
+            let amp = 1.0 / (1.0 + (distance).powi(2));
+            println!("Distance: {}, Amplitude: {}", distance, amp);
+            amplitude.set_value(amp);
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(5));
+    }
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> (f32, f32))
+where
+    T: SizedSample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let sample = next_sample();
+        let left = T::from_sample(sample.0);
+        let right = T::from_sample(sample.1);
+
+        for (channel, sample) in frame.iter_mut().enumerate() {
+            if channel & 1 == 0 {
+                *sample = left;
+            } else {
+                *sample = right;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,166 +1,100 @@
-//! Process (stereo) input and play the result (in stereo).
-
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{FromSample, SizedSample};
-use fundsp::hacker32::*;
-
-use crossbeam_channel::{bounded, Receiver, Sender};
+use cpal::traits::{DeviceTrait, HostTrait};
+use crossbeam_channel::bounded;
 use macroquad::prelude::*;
-
-#[derive(Clone)]
-pub struct InputNode {
-    receiver: Receiver<(f32, f32)>,
-}
-
-impl InputNode {
-    pub fn new(receiver: Receiver<(f32, f32)>) -> Self {
-        InputNode { receiver }
-    }
-}
-
-impl AudioNode for InputNode {
-    const ID: u64 = 87;
-    type Inputs = U0;
-    type Outputs = U2;
-
-    #[inline]
-    fn tick(&mut self, _input: &Frame<f32, Self::Inputs>) -> Frame<f32, Self::Outputs> {
-        let (left, right) = self.receiver.try_recv().unwrap_or((0.0, 0.0));
-        [left, right].into()
-    }
-}
-
-fn run_in<T>(device: &cpal::Device, config: &cpal::StreamConfig, sender: Sender<(f32, f32)>)
-where
-    T: SizedSample,
-    f32: FromSample<T>,
-{
-    let channels = config.channels as usize;
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-    let stream = device.build_input_stream(
-        config,
-        move |data: &[T], _: &cpal::InputCallbackInfo| read_data(data, channels, sender.clone()),
-        err_fn,
-        None,
-    );
-    if let Ok(stream) = stream {
-        if let Ok(()) = stream.play() {
-            std::mem::forget(stream);
-        }
-    }
-    println!("Input stream built.");
-}
-
-fn read_data<T>(input: &[T], channels: usize, sender: Sender<(f32, f32)>)
-where
-    T: SizedSample,
-    f32: FromSample<T>,
-{
-    for frame in input.chunks(channels) {
-        let mut left = 0.0;
-        let mut right = 0.0;
-        for (channel, sample) in frame.iter().enumerate() {
-            if channel & 1 == 0 {
-                left = sample.to_sample::<f32>();
-            } else {
-                right = sample.to_sample::<f32>();
-            }
-        }
-        if let Ok(()) = sender.try_send((left, right)) {}
-    }
-}
-
-fn run_out<T>(device: &cpal::Device, config: &cpal::StreamConfig, receiver: Receiver<(f32, f32)>)
-where
-    T: SizedSample + FromSample<f32>,
-{
-    let channels = config.channels as usize;
-
-    let input = An(InputNode::new(receiver));
-    let reverb = reverb2_stereo(20.0, 3.0, 1.0, 0.2, highshelf_hz(1000.0, 1.0, db_amp(-1.0)));
-    let chorus = chorus(0, 0.0, 0.03, 0.2) | chorus(1, 0.0, 0.03, 0.2);
-    // Here is the final input-to-output processing chain.
-    let graph = input;
-    let mut graph = BlockRateAdapter::new(Box::new(graph));
-    graph.set_sample_rate(config.sample_rate.0 as f64);
-
-    let mut next_value = move || graph.get_stereo();
-
-    let err_fn = |err| eprintln!("An error occurred on stream: {}", err);
-    let stream = device.build_output_stream(
-        config,
-        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-            write_data(data, channels, &mut next_value)
-        },
-        err_fn,
-        None,
-    );
-    if let Ok(stream) = stream {
-        if let Ok(()) = stream.play() {
-            std::mem::forget(stream);
-        }
-    }
-    println!("Output stream built.");
-}
-
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> (f32, f32))
-where
-    T: SizedSample + FromSample<f32>,
-{
-    for frame in output.chunks_mut(channels) {
-        let sample = next_sample();
-        let left = T::from_sample(sample.0);
-        let right = T::from_sample(sample.1);
-
-        for (channel, sample) in frame.iter_mut().enumerate() {
-            if channel & 1 == 0 {
-                *sample = left;
-            } else {
-                *sample = right;
-            }
-        }
-    }
-}
+use std::sync::{Arc, RwLock};
+use voice_immersion::{run_in, run_out, SourceInfo, HEAD_RADIUS};
 
 #[macroquad::main("3D")]
-async fn main() {
-    let mut player_pos = vec3(-8., 0., 0.);
+async fn main() -> anyhow::Result<()> {
+    let source_info = Arc::new(RwLock::new(SourceInfo::default()));
+    let source_info_audio = source_info.clone();
+    std::thread::spawn(move || {
+        // Sender / receiver for left and right channels (stereo mic).
+        let (sender, receiver) = bounded(4096 * 4);
+
+        let host = cpal::default_host();
+        // Start input.
+        let in_device = host.default_input_device().unwrap();
+        let in_config = in_device.default_input_config().unwrap();
+        match in_config.sample_format() {
+            cpal::SampleFormat::F32 => run_in::<f32>(&in_device, &in_config.into(), sender),
+            cpal::SampleFormat::I16 => run_in::<i16>(&in_device, &in_config.into(), sender),
+            cpal::SampleFormat::U16 => run_in::<u16>(&in_device, &in_config.into(), sender),
+            format => eprintln!("Unsupported sample format: {}", format),
+        }
+
+        // Start output.
+        let out_device = host.default_output_device().unwrap();
+        let out_config = out_device.default_output_config().unwrap();
+        match out_config.sample_format() {
+            cpal::SampleFormat::F32 => {
+                let _ =
+                    run_out::<f32>(&out_device, &out_config.into(), receiver, source_info_audio);
+            }
+            cpal::SampleFormat::I16 => {
+                let _ =
+                    run_out::<i16>(&out_device, &out_config.into(), receiver, source_info_audio);
+            }
+            cpal::SampleFormat::U16 => {
+                let _ =
+                    run_out::<u16>(&out_device, &out_config.into(), receiver, source_info_audio);
+            }
+            format => eprintln!("Unsupported sample format: {}", format),
+        }
+    });
+
+    println!("Processing stereo input to stereo output.");
+
+    let mut player_pos = vec3(-2., 0., 0.);
+
     loop {
         clear_background(LIGHTGRAY);
 
         // Camera
         set_camera(&Camera3D {
-            position: vec3(-20., 15., 0.),
+            position: vec3(-10., 5., 0.),
             up: vec3(0., 1., 0.),
             target: vec3(0., 0., 0.),
             ..Default::default()
         });
 
-        draw_grid(20, 1., BLACK, GRAY);
+        draw_grid(10, 1., BLACK, GRAY);
 
-        /* Source */ 
-        draw_sphere(vec3(0., 0., 0.), 1., None, BLACK);
+        /* Source */
+        draw_sphere(vec3(0., 0., 0.), HEAD_RADIUS, None, BLACK);
 
         /* Player */
         if is_key_down(KeyCode::Left) {
-            player_pos.z -= 0.1;
+            player_pos.z -= 0.01;
         }
         if is_key_down(KeyCode::Right) {
-            player_pos.z += 0.1;
+            player_pos.z += 0.01;
         }
         if is_key_down(KeyCode::Up) {
-            player_pos.x += 0.1;
+            player_pos.x += 0.01;
         }
         if is_key_down(KeyCode::Down) {
-            player_pos.x -= 0.1;
+            player_pos.x -= 0.01;
         }
 
-        draw_sphere(player_pos, 1., None, BLUE);
+        let a = source_info.try_write();
 
-        // Back to screen space, render some text
+        if let Ok(mut source_info) = a {
+            source_info.relative_position.x = player_pos.x;
+            source_info.relative_position.y = player_pos.y;
+            source_info.relative_position.z = player_pos.z;
+        }
+
+        draw_sphere(player_pos, HEAD_RADIUS, None, BLUE);
 
         set_default_camera();
-        draw_text("WELCOME TO 3D WORLD", 10.0, 20.0, 30.0, BLACK);
+        draw_text(
+            &format!("{:?}", source_info.read().unwrap().relative_position),
+            10.0,
+            20.0,
+            30.0,
+            BLACK,
+        );
 
         next_frame().await
     }
